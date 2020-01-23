bc5a8b5022814e074b485d6e901634b26b558ad5
userTest <1158809285@qq.com>
2020-01-24 02:03:49 +0800

    初始化spring-cloud消费者及生产者

---
+++ SpringCloud-Server-Test/src/main/java/com/example/demo/service/AdminService.java
@@ -0,0 +1,17 @@
+package com.example.demo.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String sayHi(String message) {
+        return restTemplate.getForObject("http://HELLO-SPRING-CLOUD-SERVICE-ADMIN/hi?message=" + message, String.class);
+    }
+}
