4bae8b3b7c3bb343b83927b538d79da339efe11d
kaleldo <1158809285@qq.com>
2020-02-15 11:31:08 +0800

    添加服务网关

---
+++ SpringCloud-gateway/src/main/java/com/kaleldo/gateway/SpringCloudGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.kaleldo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SpringCloudGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewayApplication.class, args);
+    }
+}
